f4e72cdc6246403da7153112fdc63083fdc80568
Finnfalter <finnfalter@gmail.com>
2020-12-09 14:45:36 +0100

    additional environment variable

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  const constraint = process.env.TIME;
+
   while(true) {
-    console.log('Containers rule!');
+    console.log(`Microservices rock! ${constraint}`);
     await sleep(5000);
   }
 }
